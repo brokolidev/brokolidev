f7cb016a05b12a44160819c0378e4484266bf2ac
brokoli <bocalist@gmail.com>
2020-12-21 17:07:01 +0900

    js / nav 정리

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,3 @@
 require('./bootstrap');
 
 require('alpinejs');
-
-function gotoGithub() {
-    // location.href="https://github.com/brokolidev"
-    console.log("test");
-}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,6 @@
             <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
                 <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
                     <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="/">BROKOLIDEV</a>
-                    <button
-                        class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                        type="button"
-                        onclick="toggleNavbar('example-collapse-navbar')"
-                    >
-                        <i class="text-white fas fa-bars"></i>
-                    </button>
                 </div>
             </div>
         </nav>
