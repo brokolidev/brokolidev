9fb35978c0c7c8be5b4b67ee87e8ad85e228776a
brokoli <brokoli@marketboro.com>
2022-09-16 15:40:36 +0900

    changed slack bot token

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function(){
             (new SlackController)->sendLunchMenuMessage('C042G9KRKFY'); // 콜리봇 테스트 채널
-        })->dailyAt('15:30')
+        })->dailyAt('15:45')
           ->timezone('Asia/Seoul')
           ->weekdays();
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/slack/recommend-menu/{chatId}', [SlackController::class => 'sendLunchMenuMessage']);
+Route::get('/slack/recommend-menu/{chatId}', [SlackController::class, 'sendLunchMenuMessage']);
 
 Route::get('/memory-game', function () {
     return view('samples.memory-game');
