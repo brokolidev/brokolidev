224cbc12473222a55e9ebaeef90a75b5ebcc07a7
brokoli <brokoli@marketboro.com>
2022-09-23 13:51:38 +0900

    handle navigation state

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,9 @@
     @vite('resources/js/app.js')
 </head>
 <body class="text-gray-800 antialiased">
+@php
+    $isActive = true;
+@endphp
 
 <header class="pointer-events-none relative z-50 flex flex-col">
     <div class="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"></div>
@@ -63,10 +66,16 @@
                                 <nav class="pointer-events-auto hidden md:block">
                                     <ul class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
                                         <li>
-                                            <a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                                               href="/">About</a></li>
+                                            <a @class([
+                                                'text-teal-500' => url()->current() === url('/'),
+                                                'relative', 'block px-3', 'py-2 transition',
+                                                'hover:text-teal-500', 'dark:hover:text-teal-400'])
+                                               href="/" >About</a></li>
                                         <li>
-                                            <a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                                            <a @class([
+                                                'text-teal-500' => url()->current() === url('/articles'),
+                                                'relative', 'block px-3', 'py-2 transition',
+                                                'hover:text-teal-500', 'dark:hover:text-teal-400'])
                                                href="/articles">Articles</a></li>
                                     </ul>
                                 </nav>
