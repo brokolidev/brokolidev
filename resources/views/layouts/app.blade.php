1d8969f0455bcd4546cf46ee96c88506367b873f
brokoli <brokoli@marketboro.com>
2022-09-23 14:56:57 +0900

    added preparing darkmode alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,8 +82,9 @@
                                     </ul>
                                 </nav>
                             </div>
-                            <div class="flex justify-end md:flex-1">
-                                <div class="pointer-events-auto">
+                            {{-- PC Dark mode button --}}
+                            <div class="flex justify-end md:flex-1" x-data="{}">
+                                <div class="pointer-events-auto" @click.prevent="alert('다크모드 준비중입니다!')">
                                     <button type="button" aria-label="Toggle dark mode"
                                             class="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20">
                                         <svg viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round"
