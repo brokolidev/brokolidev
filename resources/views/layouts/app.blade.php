9b685cfe4d45c37fc96d5d650dbe066522310c8d
brokolidev <bocalist@gmail.com>
2024-06-15 10:55:55 -0600

    changed nav bar status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,15 +53,13 @@
                                     <nav class="pointer-events-auto hidden md:block">
                                         <ul class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
                                             <li>
-                                                <a @class([ 'text-teal-500'=> url()->current() === url('/'),
-                                                    'dark:text-teal-400' => url()->current() === url('/'),
+                                                <a @class([ 'text-teal-500 dark:text-teal-400' => request()->is('/'),
                                                     'relative', 'block px-3', 'py-2 transition',
                                                     'hover:text-teal-500', 'dark:hover:text-teal-400'])
                                                     href="/" >About</a>
                                             </li>
                                             <li>
-                                                <a @class([ 'text-teal-500'=> url()->current() === url('/articles'),
-                                                    'dark:text-teal-400' => url()->current() === url('/articles'),
+                                                <a @class([ 'dark:text-teal-400 text-teal-500'=> request()->is('articles') || request()->is('articles/*'),
                                                     'relative', 'block px-3', 'py-2 transition',
                                                     'hover:text-teal-500', 'dark:hover:text-teal-400'])
                                                     href="/articles">Articles</a>
