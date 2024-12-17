57d1223df2525aa57d368c7f0874671ae70ee9a2
brokolidev <bocalist@gmail.com>
2024-12-17 11:24:09 -0700

    removed prismjs setting from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import prismjs from 'vite-plugin-prismjs';
 
 export default defineConfig({
     plugins: [
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
-        ]),
-        prismjs({
-            languages: 'all',
-        })
+        ])
     ],
 });
